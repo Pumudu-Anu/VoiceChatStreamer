2e3fdb051badb161c817e7a657a297dd18ea22ba
Pumudu-Anu <86913050+Pumudu-Anu@users.noreply.github.com>
2021-10-31 05:42:21 +0530

    Update config.py
    hiii

--- config.py
+++ config.py
@@ -4,19 +4,19 @@
 CONFIG = False
 
 # Add your APP ID from my.telegram.org
-API_ID = 123456
+API_ID = 4472818
 
 # Add your API HASH from my.telegram.org
-API_HASH = "xxxxxxxxxxxxxxxxxxx"
+API_HASH = "fe5f0e7c21f255af922ba4d3b2c74967"
 
 # Read readme and generate string session. Then add that to here.
-SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SESSION = "AQAfS1GmLOtYIN9LvrH84vYaUcCAoWE-3rFVAgLqqTYJ9ZjiHKQXYYIQd7uAhz2obVsLPrMHFnQbbEGmmbBpZTEa70FqwzkCzcUj8dkssvVYkiq3uLQDiLPDCqXNLheX3S62UWO1dHCW-Y2RpEaAKlvn46jY12sA6XbV25lkQe5ygp-d9wJWigr87smpzj9rlCr5IvHRsejFRiJCWqWfVnwHKSKTsn0KyNDOvaqwQOUkmRqeSPtO1cQMnLmhjO4-46V7Uj-9ZHBm13lV7iCUTHJzh9VxRdtet9FND5Icb_m6EKlQhRU3phzp0a4k6JzPFMli0rbPelO24IkncXgzYDJWewnIUAA"
 
 # Add your bot token here
-TOKEN = "12345678:xxxxxxxxxxxxxxxxxxxxxxxx"
+TOKEN = "2071532691:AAGE1e-vTqiOVXvGeAmgPpyCOruPpl9paTA"
 
 # Chat id where bot should work.
-CHAT_ID = -10010000000
+CHAT_ID = -1001501392115
 
 # Add ur TG ID, Seperate by spaces
-ADMINS = 123456789
+ADMINS = 1759889581
